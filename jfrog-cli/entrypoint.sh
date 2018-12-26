f3017b0a9b19c2a738034a34a65e0bc6f1a46536
retgits <lstigter@gmail.com>
2018-12-26 13:08:03 -0800

    Update dockerfile and entrypoint for JFrog CLI

--- jfrog-cli/entrypoint.sh
+++ jfrog-cli/entrypoint.sh
@@ -5,15 +5,15 @@ set -e
 if [ -z ${USER+x} ]; 
 then 
     echo "Authentication using username/password";
-    sh -c "jfrog rt $COMMAND --url $URL --user $USER --password $PASSWORD $COMMANDOPTS $COMMANDARGS"
+    sh -c "jfrog rt $COMMAND --url=$URL --user=$USER --password=$PASSWORD $COMMANDOPTS $COMMANDARGS"
 elif [ -z ${APIKEY+x} ];
 then 
     echo "Authentication using API key";
-    sh -c "jfrog rt $COMMAND --apikey $APIKEY $COMMANDOPTS $COMMANDARGS"
+    sh -c "jfrog rt $COMMAND --apikey=$APIKEY $COMMANDOPTS $COMMANDARGS"
 elif [ -z ${ACCESSTOKEN+x} ];
 then 
     echo "Authentication using Access Token";
-    sh -c "jfrog rt $COMMAND --access-token $ACCESSTOKEN $COMMANDOPTS $COMMANDARGS"
+    sh -c "jfrog rt $COMMAND --access-token=$ACCESSTOKEN $COMMANDOPTS $COMMANDARGS"
 else 
     echo "ERROR! No authentication has been set :("; 
 fi
